d61431dce0b12bff3c009f36632d7a4d7032707a
dickbrmly <dickbrmly@outlook.com>
2021-09-11 04:14:30 -0700

    first commit

---
+++ state.js
@@ -0,0 +1,13 @@
+class Calculator {
+	constructor() {
+		this.mode = 'down';
+		this.position = 0;
+		this.func = 'add'
+		this.x = 0;
+		this.y = 0;
+		this.result = 0;
+	}
+};
+export {
+	Calculator
+};
