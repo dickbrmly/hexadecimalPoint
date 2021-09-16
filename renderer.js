a6eb9d7975801f46f206b15afac1fe016d67011b
dickbrmly <dickbrmly.outlook.com>
2021-09-16 06:01:53 -0700

    added ids

--- renderer.js
+++ renderer.js
@@ -5,11 +5,43 @@
 // selectively enable features needed in the rendering
 // process.
 
-import {
-	Calculator
-} from './state.js';
+import { Calculator } from './state.js';
 
 var state = new Calculator();
 
-if (state.mode === 'up') document.getElementById('multiply').innerHTML = "X<sup>Y</sup>";
-else document.getElementById('multiply').innerHTML = "*";
+//if (state.mode === 'up') document.getElementById('multiply').innerHTML = "X<sup>Y</sup>";
+//lse document.getElementById('multiply').innerHTML = "*";
+
+document.getElementById("shift").addEventListener("click", shift);
+document.getElementById("littleEndian").addEventListener("click", shift);
+
+document.getElementById("int").addEventListener("click", shift);
+document.getElementById("bin").addEventListener("click", modeBin);
+
+function shift()
+{
+    if (state.mode === 'down')
+    {
+        state.mode = 'up';
+        document.getElementById("displayShift").style.backgroundColor = 'white';
+    }
+    else
+    {
+        state.mode = 'down';
+        document.getElementById("displayShift").style.backgroundColor = 'gray';
+    }
+}
+
+function modeBin()
+{
+    if (state.display === 'bin')
+    {
+        state.display = 'normal';
+        document.getElementById("displayBinary").style.backgroundColor = 'gray';
+    }
+    else
+    {
+        state.display = 'bin';
+        document.getElementById("displayBinary").style.backgroundColor = 'white';
+    }
+}
--- state.js
+++ state.js
@@ -1,13 +1,15 @@
-class Calculator {
-	constructor() {
-		this.mode = 'down';
-		this.position = 0;
-		this.func = 'add'
-		this.x = 0;
-		this.y = 0;
-		this.result = 0;
-	}
-};
-export {
-	Calculator
-};
+class Calculator
+{
+    constructor()
+    {
+        this.mode = 'down';
+        this.entry = 'decimal';
+        this.display = 'decimal';
+        this.position = 0;
+        this.func = 'add'
+        this.x = 0;
+        this.y = 0;
+        this.result = 0;
+    }
+}
+export { Calculator };
